1647a07e302e9ab421e2ff6a428315dfa2af33e1
shivaniparteki <shivaniparteki39@gmail.com>
2024-04-02 15:57:43 +0530

    added navbar as image

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import navBar from '../assets/images/soekwerk_nav.jpeg'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+      <div className=''>
+
+        <img className='w-full h-[180px]' src={navBar} alt="" />
+      </div>
+    </>
   )
 }
 
