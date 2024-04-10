04567f0625ffd3d0b9537095600992b898304dcc
kalilinux-png <kalilinuxuser5@gmail.com>
2024-04-10 15:18:52 +0530

    fix:schemes in doc

--- backend/swagger.js
+++ backend/swagger.js
@@ -27,7 +27,7 @@ const routeFiles = getRouteFiles();
 console.log(routeFiles);
 
 const swaggerAutogen = require('swagger-autogen')();
-const HOST =   process.env.ENVIROMENT === 'production' ? 'https://soekwerk.onrender.com/' : 'localhost'
+const HOST =   process.env.ENVIROMENT === 'production' ? 'https://soekwerk.onrender.com' : 'localhost'
 const PORT =  process.env.ENVIROMENT === 'production' ? '' : ':5000';
 
 console.log("host and port",HOST,PORT)
